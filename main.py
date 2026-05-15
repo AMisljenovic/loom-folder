a6c7b0ee7ca12b8a8a0be27addece076f13ae536
AMisljenovic <44008105+AMisljenovic@users.noreply.github.com>
2026-05-15 19:19:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def main() -> None:
+    result = add(2, 3)
+    print(f"Hello from your simple Python project! 2 + 3 = {result}")
+
+
+if __name__ == "__main__":
+    main()
